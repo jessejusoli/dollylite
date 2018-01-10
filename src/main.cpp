b2fe604db0a6ab4c926958addca25caf3bccadf1
datacenter <jessejusoli@gmail.com>
2018-01-10 16:08:36 +0000

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -2806,7 +2806,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0x..."));
         
         
         if (true && block.GetHash() != hashGenesisBlock)
